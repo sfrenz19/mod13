3b84883cfc4187efc3d1f23bc691426a05e9c189
Sarah Frenz <sfrenz19@bsmvwgfwkair.bsm-online.local>
2018-04-04 13:46:35 -0500

    setup

---
+++ beginning.py
@@ -0,0 +1,21 @@
+import setup
+from setup import RPL
+import time
+start = time.time()
+
+x = 0
+while x <= 5:
+    start = time.time
+    z = 7
+    while z == 7:
+        current = time.time
+        if current - start < 50:
+            RPL.servoWrite(1,500)
+            RPL.servoWrite(0,2500)
+        elif current - start > 50 and current - start < 100:
+            RPL.servoWrite(1,2500)
+            RPL.servoWrite(0,500)
+        else:
+            z = 9
+    x +=1
+    print "loop %s" % x
