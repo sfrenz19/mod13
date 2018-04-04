354e3200fd896be0df42203278952651fc2145dc
Sarah Frenz <sfrenz19@bsmvwgfwkair.bsm-online.local>
2018-04-04 14:16:11 -0500

    m

--- beginning.py
+++ beginning.py
@@ -9,13 +9,14 @@ while x <= 5:
     z = 7
     while z == 7:
         current = time.time()
+
         if current - start < 50:
             RPL.servoWrite(1,500)
             RPL.servoWrite(0,2500)
         elif current - start > 50 and current - start < 100:
             RPL.servoWrite(1,2500)
             RPL.servoWrite(0,500)
-        else:
+        if current - start > 100:
             z = 9
     x +=1
     print "loop %s" % x
