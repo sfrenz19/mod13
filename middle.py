834d22a0013b8d930aca83d0d5fb83b8630519d7
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-04-20 13:31:24 -0500

    Update middle.py

--- middle.py
+++ middle.py
@@ -6,10 +6,10 @@ left = 1
 right = 0
 while z == 3:
     RPL.servoWrite(1,2500)
-    RPL.servoWrite(0,1500)
+    RPL.servoWrite(0,1499)
     while RPL.readDistance(rear) > 70000:
         RPL.servoWrite(1,2500)
-        RPL.servoWrite(0,1500)
+        RPL.servoWrite(0,1499)
     while RPL.readDistance(rear) > 40000:
         RPL.servoWrite(1,1500)
         RPL.servoWrite(0,500)
