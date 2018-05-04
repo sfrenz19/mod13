1bb2f2f16b4b0f20504df8966a581d48e83adc44
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2018-05-04 13:41:14 -0500

    s

--- middlenoglitch.py
+++ middlenoglitch.py
@@ -5,10 +5,14 @@ rear = 17
 left = 1
 right = 0
 while z == 3:
-    while RPL.readDistance(rear) > 70000:
+    if RPL.readDistance(rear) < 1000:
+        RPL.servoWrite(0,0)
+        RPL.servoWrite(1,0)
+        z = 5
+    if RPL.readDistance(rear) > 70000:
         RPL.servoWrite(1,2500)
         RPL.servoWrite(0,1499)
-    while RPL.readDistance(rear) > 40000:
+    if RPL.readDistance(rear) > 40000:
         RPL.servoWrite(1,1500)
         RPL.servoWrite(0,500)
 
